1fe68c074b29dbbf33244d287079948dd45cb444
Yang Liu <summerliu2333@gmail.com>
2026-02-28 13:34:53 -0800

    Add Print() & Test Object

--- lab14.cpp
+++ lab14.cpp
@@ -1,5 +1,6 @@
 //COMSC-210-5068, Lab 14, Yang Liu
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 class Color {
@@ -15,8 +16,31 @@ public:
     int getRed() const { return red; }
     int getGreen() const { return green; }
     int getBlue() const { return blue; }
+
+    void print() const {
+        cout << "RGB(" 
+            << setw(3) << setfill(' ') << red << ", "
+                  << setw(3) << setfill(' ') << green << ", "
+                  << setw(3) << setfill(' ') << blue << ")" 
+                  << endl;
+    }
 };
 
 int main() {
+    Color black;
+    black.print();
+
+    Color red;
+    red.setRed(255);
+    red.setGreen(0);
+    red.setBlue(0);
+    red.print();
+
+    Color gray;
+    gray.setRed(128);
+    gray.setGreen(128);
+    gray.setBlue(128);
+    gray.print();
+
     return 0;
 }
