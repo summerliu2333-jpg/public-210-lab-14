3174f8b1ab2ac46e789991b52a20ffd0e26a4414
Yang Liu <summerliu2333@gmail.com>
2026-02-28 13:23:47 -0800

    Add Setters &  Getters

--- lab14.cpp
+++ lab14.cpp
@@ -2,6 +2,21 @@
 #include <iostream>
 using namespace std;
 
+class Color {
+private:
+    int red, green, blue; 
+public:
+    Color() : red(0), green(0), blue(0) {} 
+
+    void setRed(int r) { red = r; }
+    void setGreen(int g) { green = g; }
+    void setBlue(int b) { blue = b; }
+
+    int getRed() const { return red; }
+    int getGreen() const { return green; }
+    int getBlue() const { return blue; }
+};
+
 int main() {
     return 0;
 }
